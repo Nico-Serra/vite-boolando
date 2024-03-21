1b655d0ad7dbf02371864bda5cabd17b192f8b58
Nico-Serra <nico.serra000@gmail.com>
2024-03-21 15:39:32 +0100

    add AppHeader component and add sass to app

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,15 +9,15 @@ export default{
     <header id="site_header">
         <div class="container d-flex">
             <ul class="gender d-flex">
-                <li class="padding-left">Donna</li>
-                <li class="padding-left">Uomini</li>
-                <li class="padding-left">Bambini</li>
+                <li class="padding-left"><a href="">Donna</a></li>
+                <li class="padding-left"><a href="">Uomini</a></li>
+                <li class="padding-left"><a href="">Bambini</a></li>
             </ul>
             <img src="/img/boolean-logo.png" alt="" class="logo">
             <ul class="icon d-flex">
-                <li class="padding-right"><i class="fa-regular fa-user"></i></li>
-                <li class="padding-right"><i class="fa-regular fa-heart"></i></li>
-                <li class="padding-right"><i class="fa-solid fa-bag-shopping"></i></li>
+                <li class="padding-right"><a href=""><i class="fa-regular fa-user"></i></a></li>
+                <li class="padding-right"><a href=""><i class="fa-regular fa-heart"></i></a></li>
+                <li class="padding-right"><a href=""><i class="fa-solid fa-bag-shopping"></i></a></li>
             </ul>
         </div>
     </header>
