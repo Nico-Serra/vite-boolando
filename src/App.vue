30d95f28407cbab160357bafc08a15a7e0fc2b3b
Nico-Serra <nico.serra000@gmail.com>
2024-03-21 15:18:06 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default{
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+
+</script>
+
+<template>
+<AppHeader/>
+
+</template>
+
+<style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<script>
+
+export default{
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <header id="site_header">
+        <div class="container d-flex">
+            <ul class="gender d-flex">
+                <li class="padding-left">Donna</li>
+                <li class="padding-left">Uomini</li>
+                <li class="padding-left">Bambini</li>
+            </ul>
+            <img src="/img/boolean-logo.png" alt="" class="logo">
+            <ul class="icon d-flex">
+                <li class="padding-right"><i class="fa-regular fa-user"></i></li>
+                <li class="padding-right"><i class="fa-regular fa-heart"></i></li>
+                <li class="padding-right"><i class="fa-solid fa-bag-shopping"></i></li>
+            </ul>
+        </div>
+    </header>
+
+</template>
+
+<style>
+
+</style>
